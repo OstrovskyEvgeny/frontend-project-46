5bdfac79af2aa1c6cd2905d2ef69fc77c7444189
Evgeniy Ostrovkiy <ostrovskiiiy22@gmail.com>
2023-04-26 16:07:19 +0400

    1. Implemented tests: describing diff functionality requirements with a yaml file. 2. Realized functionality: searching for differences between two flat (only key-value pairs) yaml files.

--- __tests__/generate-diff.test.js
+++ __tests__/generate-diff.test.js
@@ -2,19 +2,27 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import genDiff from '../src/generate-diff.js';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixtureFilePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFixtureFile = (filename) => fs.readFileSync(getFixtureFilePath(filename), 'utf-8');
 
 test('diff json files', () => {
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file2.json');
-  const diff = readFile('diff-json-files-1-2.txt');
+  const filePath1 = getFixtureFilePath('file1.json');
+  const filePath2 = getFixtureFilePath('file2.json');
+  const diff = readFixtureFile('diff-files-1-2.txt');
+
+  expect(genDiff(filePath1, filePath2)).toEqual(diff);
+});
+
+test('diff yaml files', () => {
+  const filePath1 = getFixtureFilePath('file1.yml');
+  const filePath2 = getFixtureFilePath('file2.yml');
+  const diff = readFixtureFile('diff-files-1-2.txt');
 
   expect(genDiff(filePath1, filePath2)).toEqual(diff);
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,13 +1,21 @@
-import { makeObjFromJSON } from './file-parser.js';
+import parser, { getPathToFile, getExtensionFile } from './parser.js';
 import { getDiffObject, getSortDiffObject } from './diff-object.js';
 import objectToStringConverter from './object-to-string-converter.js';
 
 export default (file1, file2) => {
-  const obj1 = makeObjFromJSON(file1);
-  const obj2 = makeObjFromJSON(file2);
+  const pathToFile1 = getPathToFile(file1);
+  const pathToFile2 = getPathToFile(file2);
+
+  const extensionFile1 = getExtensionFile(pathToFile1);
+  const extensionFile2 = getExtensionFile(pathToFile2);
+
+  const obj1 = parser(pathToFile1, extensionFile1);
+  const obj2 = parser(pathToFile2, extensionFile2);
 
   const diffObject = getDiffObject(obj1, obj2);
+
   const sortDiffObject = getSortDiffObject(diffObject);
+
   const diffObjectToString = objectToStringConverter(sortDiffObject);
 
   return diffObjectToString;
